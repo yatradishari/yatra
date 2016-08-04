e2aa2a5eaf9d55dd5b7a159d6567ce07055e84e8
uday2k9 <uday2k9>
2016-08-04 09:03:07 +0530

    code pushed

---
+++ resources/views/front/includes/footer.blade.php
@@ -0,0 +1,29 @@
+<div class="container">
+	<div class="row">
+		<div class="col-xs-4">
+			<div class="text-left">
+				&copy; Copyright Yatradishari
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<!-- Theme by <a href="http://www.themewagon.com">THEMEWAGON</a> -->
+		</div>
+		<div class="col-xs-4">
+			<div class="top">
+				<!-- <a href="#top">
+					<i class="ion-arrow-up-b"></i>
+				</a> -->
+			</div>
+		</div>
+	</div>
+</div>	
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-76571750-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
